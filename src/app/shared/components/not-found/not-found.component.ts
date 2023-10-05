b11984d0126e4ef2632b162f655717917ff4b690
Ahmad Shabeer K <ahmadshabeerk786@gmail.com>
2023-10-05 23:31:36 +0530

    feat: added not found page(completely unmatched design)

--- src/app/shared/components/not-found/not-found.component.ts
+++ src/app/shared/components/not-found/not-found.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import * as GlobalActions from '../../../shared/store/actions';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.css']
+  styleUrls: ['./not-found.component.css'],
 })
 export class NotFoundComponent {
+  store = inject(Store);
 
+  constructor() {
+    this.store.dispatch(GlobalActions.setHeader({ header: 'Not Found' }));
+  }
 }
